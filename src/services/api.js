c0c4b29bfe3ac96f7029ceec252c622c7a9fdee3
jsican-2023091 <jsican-2023091@kinal.edu.gt>
2025-05-16 01:09:10 -0600

    Se agregan nuevas funciones al api

--- src/services/api.js
+++ src/services/api.js
@@ -11,32 +11,45 @@ export const getPostRequest = async() => {
     try {
         return await apiClient.get('/post/getAll')
     } catch (err) {
-        return{
-            error: true,
-            err
-        }
+        return{ error: true, err }
     }
 } 
 
-export const getPostById = async (id) => {
+export const getPostById = async (postId) => {
   try {
-    return await apiClient.get(`/post/${id}`)
+    return await apiClient.get(`/post/getPostById/${postId}`)
   } catch (err) {
     return { error: true, err }
   }
 }
 
-export const getCommentsByPostId = async (postId) => {
+export const getCommentsById = async (commentid) => {
   try {
-    return await apiClient.get(`/comment/getCommentsByPost/${postId}`)
+    return await apiClient.get(`/comment/getCommentsById/${commentid}`)
   } catch (err) {
     return { error: true, err }
+  }
+}
+
+export const getCommentsByPostId = async(postId) => {
+  try {
+    return await apiClient.get(`/comment/getCommentByPostId/${postId}`)
+  } catch (err) {
+    return { error: true, err}
   }
 }
 
 export const createComment = async (data) => {
   try {
-    return await apiClient.post('/save', data)
+    return await apiClient.post('/comment/save', data)
+  } catch (err) {
+    return { error: true, err }
+  }
+}
+
+export const getPostsByCourse = async (course) => {
+  try {
+    return await apiClient.get(`/post/getCourse/${encodeURIComponent(course)}`)
   } catch (err) {
     return { error: true, err }
   }
